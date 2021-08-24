f978a9e6bd2d5edd588a742fd00e55498b912fef
imsaptarshi <123saptarshi.basu@gmail.com>
2021-08-24 18:05:23 +0530

    init

--- plant_growth_prediction/gdd.py
+++ plant_growth_prediction/gdd.py
@@ -1,37 +1,38 @@
 import csv
 import math
 
-FILE="data.csv"
-FIELDS=["date","temperature"]
+FILE = "data.csv"
+FIELDS = ["date", "temperature"]
+
 
 class GDD:
-    def __init__(self,required_gdd:int,base_temperature:float):
-        self.required_gdd=required_gdd
-        self.base_temperature=base_temperature
-    
-    def calculate_gdd_received(self):#gdd received_per_day
-        min=100
-        max=0
+    def __init__(self, required_gdd: int, base_temperature: float):
+        self.required_gdd = required_gdd
+        self.base_temperature = base_temperature
+
+    def calculate_gdd_received(self):  # gdd received_per_day
+        min = 100
+        max = 0
         with open(FILE) as csv_file:
-            dict_reader=csv.DictReader(csv_file)
+            dict_reader = csv.DictReader(csv_file)
             for row in dict_reader:
-                temp=row["temperature"]
+                temp = row["temperature"]
 
-                if float(temp)<min:
-                    min=float(temp)
+                if float(temp) < min:
+                    min = float(temp)
 
-                if float(temp)>max:
-                    max=float(temp)
+                if float(temp) > max:
+                    max = float(temp)
 
-        self.gdd=math.ceil((max+min)/2 - self.base_temperature)
+        self.gdd = math.ceil((max+min)/2 - self.base_temperature)
         return self.gdd
 
     def predict_number_of_days(self):
         return math.ceil(self.required_gdd/self.gdd)
 
 
-if __name__=="__main__":
-    #example
-    corn=GDD(400,10)
+if __name__ == "__main__":
+    # example
+    corn = GDD(400, 10)
     print(corn.calculate_gdd_received())
     print(corn.predict_number_of_days())
